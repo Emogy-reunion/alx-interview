3759290b0c2d510c9da5978ba53b7e4bf362bf1d
Emogy-reunion <mv7786986@gmail.com>
2024-08-23 17:12:12 +0300

    first commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,28 @@
+#!/usr/bin/python3
+'''
+The coin problem
+'''
+import sys
+
+
 def makeChange(coins, total):
+    '''
+    Returns the fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
     if total <= 0:
         return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    # Create an array to store the minimum coins needed for each value up to total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: no coins needed to make 0
-
-    # Iterate through each coin
-    for coin in coins:
-        for i in range(coin, total + 1):
-            # Update dp[i] if using the coin results in fewer coins
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # If dp[total] is still infinity, then it's not possible to make the total
-    return dp[total] if dp[total] != float('inf') else -1
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
