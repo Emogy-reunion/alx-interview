c5d6c61ab7b8772d950a64e79debb3ff3e4e2ed5
Emogy-reunion <mv7786986@gmail.com>
2024-09-05 18:05:18 +0300

    first commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Prime game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Determines the winner of a prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
